c71cecc389e5ff2273058e43c9437a6665d357b7
Fernando Fernandez Ortiz <fernando.fernandezo@edu.uah.es>
2023-03-20 19:10:12 +0000

    Añadidos cambios en servidor para introducir por linea de comandos si se desea conexion persitente o no, HTTP o HTTPS. Error en HTTPS, revisar

--- server.py
+++ server.py
@@ -8,16 +8,24 @@ import platform
 import mimetypes
 import string
 import pymysql
-import requests
-import base64
+import ssl
 
-#Conexion con Base de datos
+# Conexion con Base de datos
+
+if sys.argv[1] == "Persistente":
+    Tipo_Conexion = "Keep-Alive"
+    Keep_Alive = "timeout=10, max=1000"
+
+if sys.argv[1] == "No-Persistente":
+    Tipo_Conexion = "close"
+    Keep_Alive = " "
+
 
 Conexion_SQL = pymysql.connect(host='localhost', user='fernando', passwd='Fernando0810_', db='LRSS2')
 cur = Conexion_SQL.cursor()
 
 # Establecer el host y el puerto del servidor
-server_address = ('192.168.189.64', 8080)
+server_address = ('192.168.2.4', 8080)
 
 # Crear un objeto socket
 try:
@@ -36,14 +44,27 @@ except Exception as e:
 
 # Escuchar por conexiones entrantes
 socketserv.listen(5)
+
+if sys.argv[2] == "HTTPS":
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile='server.cert', keyfile='server.key')
+    secure_socket = ssl_context.wrap_socket(socketserv, server_side=True)
+
+
 print('Escuchando en ip ', server_address[0], 'y puerto:', server_address[1])
 
-entradas = [socketserv] #Sockets de los que esperamos hacer lectura
-salidas = [] # Sockets sobre los que esperamos escribir
-cola_mensajes = {} # Buffer de salida para conexiones de salida
+if sys.argv[2] == "HTTP":
+    entradas = [socketserv]  # Sockets de los que esperamos hacer lectura
+elif sys.argv[2] == "HTTPS":
+    entradas = [secure_socket]
 
-# Cargamos el contenido del archivo HTML que se va a solicitar al servidor en una cadena.
+salidas = []  # Sockets sobre los que esperamos escribir
+cola_mensajes = {}  # Buffer de salida para conexiones de salida
 
+if sys.argv[2] == "HTTP":
+    Descriptor_socket_serv = socketserv
+elif sys.argv[2] == "HTTPS":
+    Descriptor_socket_serv = secure_socket
 
 while entradas != -1:
     print('Esperando peticiones entrantes...\n')
@@ -52,14 +73,9 @@ while entradas != -1:
         print("Servidor desconectado debido a temporizador 'timeout'")
 
     for s in readable:
-        if s is socketserv:
+        if s is Descriptor_socket_serv:
             # Un socket en modo lectura esta preparado para aceptar la conexion
-            try:
-                conexion, client_address = socketserv.accept()
-            except Exception as e:
-                print("Error conectando servidor con el cliente: ", e)
-                
-            print("Conexión procedente de ", client_address)
+            conexion, client_address = s.accept()
             conexion.setblocking(0)
             entradas.append(conexion)
 
@@ -81,7 +97,7 @@ while entradas != -1:
 
                 entradas.pop()
                 s.close()
-            
+
             else:
                 # Convertir los datos de entrada en una solicitud HTTP.
 
@@ -89,146 +105,149 @@ while entradas != -1:
                 separacion = data.decode().split(' ')
                 cabecera = separacion[0]
                 archivo_pedido = separacion[1]
-                nuevo_archivo_pedido = archivo_pedido.replace('/','')
+                nuevo_archivo_pedido = archivo_pedido.replace('/', '')
+                if len(archivo_pedido) == 1:
+
+                    nuevo_archivo_pedido = "index.html"
+
                 # Con esto obtenemos la cabecera de una peticion HTTP, dependiento del tipo
                 # de solicitud, realizaremos una acción u otra.
-                print(solicitud,"\n")
+                print(solicitud, "\n")
                 print(cabecera)
                 print(nuevo_archivo_pedido)
 
-                try: 
+                try:
+                    if nuevo_archivo_pedido == "index.html":
+                        file = open("index.html", 'r')
+                        index = file.read()
+                        file.close()
 
-                    file = open("index.html", 'r')
-                    response = file.read() 
-                    file.close()
+                    elif nuevo_archivo_pedido == "index2.html":
+
+                        file = open("index2.html", 'r')
+                        index = file.read()
+                        file.close()
+
+                    elif nuevo_archivo_pedido == "index3.html":
+
+                        file = open("index3.html", 'r')
+                        index = file.read()
+                        file.close()
 
                     file = open("css/index.css", 'r')
                     response_css = file.read()
                     file.close()
 
-                    file = open("img/redes.png", 'rb')
-                    imagen_bytes = file.read()
-                    img_data = base64.b64encode(imagen_bytes).decode("utf-8")
-                    file.close()
-                    
-                
-                    ruta_archivo_html = "index.html"
+                    if nuevo_archivo_pedido.endswith('.html'):
+                        ruta_archivo_html = nuevo_archivo_pedido
+
                     ruta_archivo_css = "css/index.css"
-                    ruta_img = "img/redes.png"
                     tamaño_html = os.path.getsize(ruta_archivo_html)
                     tamaño_css = os.path.getsize(ruta_archivo_css)
-                    tamaño_img = os.path.getsize(ruta_img)
                     tamaño_total = tamaño_css + tamaño_html
                     tamaño_total = str(tamaño_total)
-                    
-                    if(archivo_pedido.endswith('/')):
+
+                    if (archivo_pedido.endswith('/')):
                         mimetype = 'text/html'
 
-                    elif(archivo_pedido.endswith('.html')):
+                    elif (archivo_pedido.endswith('.html')):
                         mimetype = 'text/html'
 
-                    elif(archivo_pedido.endswith('.css')):
+                    elif (archivo_pedido.endswith('.css')):
                         mimetype = 'text/css'
 
-                    elif(archivo_pedido.endswith('.png')):
+                    elif (archivo_pedido.endswith('.png')):
                         mimetype = 'image/png'
 
-
                     if cabecera.startswith('GET'):
+
                         fecha_hora = datetime.datetime.now()
                         fecha_bytes = fecha_hora.strftime("%Y-%m-%d %H:%M:%S.%f")
                         plataforma = platform.system()
-                        Tipo_Conexion = "keep-alive"
-                        cabecera_1 = 'HTTP/1.1 200 OK\r\nDate: '+ fecha_bytes + '\r\nConnection: ' + Tipo_Conexion + '\r\nServidor:' + plataforma + '\r\nContent-type: ' + mimetype + '\r\nContent-length:' + tamaño_total + '\r\n\n{}'.format(response + '<style>' + response_css + '</style>')
+                        cabecera_1 = 'HTTP/1.1 200 OK\r\nDate: ' + fecha_bytes + '\r\nConnection: ' + Tipo_Conexion + '\r\nKeep-Alive: ' + Keep_Alive + '\r\nServidor:' + plataforma + '\r\nContent-type: ' + \
+                            mimetype + '; charset=utf-8\r\nContent-length:' + tamaño_total + \
+                            '\r\n\n{}'.format(index + '<style>' + response_css + '</style>')
                         print(cabecera_1)
-                        respuesta = cabecera_1.encode("utf-8")
-                        s.sendall(respuesta)
-                        s.sendall(img_data)
+                        try:
+                            s.sendall(cabecera_1.encode())
+                        except Exception as e:
+                            exceptional.append(s)
+                            print("Error al enviar los datos ", e)
+
                         entradas.pop()
                         s.close()
                         break
 
                     if cabecera.startswith('POST'):
 
-                        if nuevo_archivo_pedido == 'index2.html':
-                            datos_formulario = separacion[-1]
-                            datos_formulario = datos_formulario.split('\r')[2]
-                            datos_formulario = datos_formulario.split('&')
-                            longitud = len(datos_formulario)
-                            
-                            for i in range(longitud):
-                                if i == 0:
-                                    Dni_Formulario = datos_formulario[i].split('=')
-                                    Dni_Formulario = dict([Dni_Formulario])
-                                if i == 1:
-                                    Nombre_formulario = datos_formulario[i].split('=')
-                                    Nombre_formulario = dict([Nombre_formulario])
+                        datos_formulario = separacion[-1]
+                        datos_formulario = datos_formulario.split('\r')[2]
+                        datos_formulario = datos_formulario.split('&')
+                        longitud = len(datos_formulario)
 
-                                if i == 2:
-                                    Apellido_formulario = datos_formulario[i].split('=')
-                                    Apellido_formulario = dict([Apellido_formulario])
+                        for i in range(longitud):
+                            if i == 0:
+                                Dni_Formulario = datos_formulario[i].split('=')
+                                Dni_Formulario = dict([Dni_Formulario])
+                            if i == 1:
+                                Nombre_formulario = datos_formulario[i].split('=')
+                                Nombre_formulario = dict([Nombre_formulario])
 
-                                if i == 3:
-                                    Fecha_formulario = datos_formulario[i].split('=')
-                                    Fecha_formulario = dict([Fecha_formulario])
-                                
-                            Dni_Formulario.update(Nombre_formulario)
-                            Dni_Formulario.update(Apellido_formulario)
-                            Dni_Formulario.update(Fecha_formulario)
-                            Diccionario_Solicitud_Final = Dni_Formulario
+                            if i == 2:
+                                Apellido_formulario = datos_formulario[i].split('=')
+                                Apellido_formulario = dict([Apellido_formulario])
 
-                            Diccionario_Solicitud_Final = list(Diccionario_Solicitud_Final.values())
-                            longitud = len(Diccionario_Solicitud_Final)
+                            if i == 3:
+                                Fecha_formulario = datos_formulario[i].split('=')
+                                Fecha_formulario = dict([Fecha_formulario])
 
-                            for i in range(longitud):
-                                if i == 0:
-                                    DNI = Diccionario_Solicitud_Final[i]
-                                if i == 1:
-                                    Nombre = Diccionario_Solicitud_Final[i]
-                                if i == 2:
-                                    Apellido = Diccionario_Solicitud_Final[i]
-                                if i == 3:
-                                    Fecha = Diccionario_Solicitud_Final[i]
+                        Dni_Formulario.update(Nombre_formulario)
+                        Dni_Formulario.update(Apellido_formulario)
+                        Dni_Formulario.update(Fecha_formulario)
+                        Diccionario_Solicitud_Final = Dni_Formulario
 
+                        Diccionario_Solicitud_Final = list(Diccionario_Solicitud_Final.values())
+                        longitud = len(Diccionario_Solicitud_Final)
 
-                            sql = "INSERT INTO DatosFormulario (DNI, nombre, apellidos, fecha) VALUES (%s,%s,%s,%s)"
-                            val = (DNI,Nombre,Apellido,Fecha)
-                            cur.execute(sql,val)
-                            Conexion_SQL.commit()
-                        
-                            file = open(nuevo_archivo_pedido, 'r')
-                            contenido = file.read()
-                            file.close()
+                        for i in range(longitud):
+                            if i == 0:
+                                DNI = Diccionario_Solicitud_Final[i]
+                            if i == 1:
+                                Nombre = Diccionario_Solicitud_Final[i]
+                            if i == 2:
+                                Apellido = Diccionario_Solicitud_Final[i]
+                            if i == 3:
+                                Fecha = Diccionario_Solicitud_Final[i]
 
-                            file = open("css/index.css", 'r')
-                            contenido_css = file.read()
-                            file.close()
+                        sql = "INSERT INTO DatosFormulario (DNI, nombre, apellidos, fecha) VALUES (%s,%s,%s,%s)"
+                        val = (DNI, Nombre, Apellido, Fecha)
+                        cur.execute(sql, val)
+                        Conexion_SQL.commit()
 
-                            ruta_archivo_css = "css/index.css"
-                            tamaño_archivo = os.path.getsize(nuevo_archivo_pedido)
-                            tamaño_css = os.path.getsize(ruta_archivo_css)
-                            tamaño_total = tamaño_css + tamaño_archivo
-                            tamaño_total = str(tamaño_total)
-                            
-                            fecha_hora = datetime.datetime.now()
-                            fecha_bytes = fecha_hora.strftime("%Y-%m-%d %H:%M:%S.%f")
-                            plataforma = platform.system()
-                            Tipo_Conexion = "keep-alive"
-                            cabecera_1 = 'HTTP/1.1 200 OK\r\nDate: '+ fecha_bytes + '\r\n Connection: ' + Tipo_Conexion + '\r\nServidor:' + plataforma + '\r\nContent-type: ' + mimetype + '\r\nContent-length:' + tamaño_total + '\r\n{}'.format(contenido + '<style>' + response_css + '</style>')
-                            respuesta_final = cabecera_1
+                        fecha_hora = datetime.datetime.now()
+                        fecha_bytes = fecha_hora.strftime(
+                            "%Y-%m-%d %H:%M:%S.%f")
+                        plataforma = platform.system()
+                        cabecera_1 = 'HTTP/1.1 200 OK\r\nDate: ' + fecha_bytes + '\r\nConnection: ' + Tipo_Conexion + '\r\nKeep-Alive: ' + Keep_Alive + '\r\nServidor:' + plataforma + \
+                            '\r\nContent-type: ' + mimetype + '\r\nContent-length:' + tamaño_total + \
+                            '\r\n{}'.format(
+                                index + '<style>' + response_css + '</style>')
+                        respuesta_final = cabecera_1
+                        print(respuesta_final)
+                        try:
                             s.sendall(respuesta_final.encode("utf-8"))
-                            entradas.pop()
-                            s.close()
+                        except Exception as e:
+                            print("Error al enviar los datos ", e)
+                            exceptional.append(s)
+                        entradas.pop()
+                        s.close()
 
                     break
-                        
+
                 except Exception as e:
-                    print("error: ",e)
+                    print("error: ", e)
 
         break
-    
-    
 
     for s in exceptional:
-        print("Excepcion en el socket desde {}:{}",format(entradas[s]))
-        
+        print("Excepcion en el socket desde {}:{}", format(entradas[s]))
